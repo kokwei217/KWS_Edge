e4b38c18e72c2c6f2cb8e1a6f1e8e70a96760c62
kokwei217 <kokwei217@gmail.com>
2021-03-06 03:34:18 +0800

    Added VGG11 and kaggle 3rd place VGG architecture

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 
 
 def simple_cnn(input_shape, num_layers=12):
@@ -63,7 +63,7 @@ def cnn_tutorial(input_shape, X_train, num_layers=12):
 def VGG16(input_shape, num_layers=12):
     model = keras.models.Sequential()
     # first part
-    model.add(Conv2D(input_shape=(224, 224, 3), filters=64,
+    model.add(Conv2D(input_shape=(input_shape), filters=64,
                      kernel_size=(3, 3), padding="same", activation="relu"))
     model.add(Conv2D(filters=64, kernel_size=(3, 3),
                      padding="same", activation="relu"))
@@ -100,7 +100,101 @@ def VGG16(input_shape, num_layers=12):
     model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
     # final
     model.add(Flatten())
-    model.add(Dense(units=4096, activation="relu"))
-    model.add(Dense(units=4096, activation="relu"))
+    model.add(Dense(units=1024, activation="relu"))
+    model.add(Dense(units=1024, activation="relu"))
     model.add(layers.Dense(num_layers, activation='softmax'))
     return model
+
+
+def VGG16_twist(input_shape, num_layers=12):
+    inputs = keras.Input(shape=(input_shape))
+    # first part
+    x = Conv2D(filters=64,
+               kernel_size=(3, 3), padding="same", activation="relu")(inputs)
+    x = Conv2D(filters=64,
+               kernel_size=(3, 3), padding="same", activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # 2nd part
+    x = Conv2D(filters=128, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=128, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # 3rd part
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # 4th part
+    x = Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # 5th part
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu")(x)
+    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(x)
+    # Additional global avg + pooling layer
+    max_x = layers.GlobalMaxPooling2D()(x)
+    avg_x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Concatenate(axis=1)([max_x, avg_x])
+    # final
+    x = Flatten()(x)
+    x = Dense(units=1024, activation="relu")(x)
+    x = layers.Dropout(0.5)(x)
+    x = Dense(units=1024, activation="relu")(x)
+    x = layers.Dropout(0.25)(x)
+    out = layers.Dense(num_layers, activation='softmax')(x)
+    model = keras.Model(inputs=inputs, outputs=out, name="vgg1")
+    return model
+
+
+def VGG11(input_shape, num_layers=12):
+    model = keras.models.Sequential()
+    # first part
+    model.add(Conv2D(input_shape=(), filters=64,
+                     kernel_size=(3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # 2nd part
+    model.add(Conv2D(filters=128, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # 3rd part
+    model.add(Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=256, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # 4th part
+    model.add(Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # 5th part
+    model.add(Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(Conv2D(filters=512, kernel_size=(
+        3, 3), padding="same", activation="relu"))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
+    # final
+    model.add(Flatten())
+    model.add(Dense(units=1024, activation="relu"))
+    model.add(Dense(units=1024, activation="relu"))
+    model.add(layers.Dense(num_layers, activation='softmax'))
+    return model
+
+
+if __name__ == "__main__":
+    m = VGG16_twist((128, 128, 1))
+    print(m.summary())
